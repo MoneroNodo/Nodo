7e609f53aacca8214efcd5e300439854ab1bb2fb
interloper <15019654+intr-cx@users.noreply.github.com>
2025-07-28 15:02:40 +0200

    monero-lws: Bandaid for outdated version

--- home/nodo/execScripts/monero-lws.sh
+++ home/nodo/execScripts/monero-lws.sh
@@ -6,4 +6,8 @@
 DATA_DIR=$(getvar "data_dir")/light_wallet_server
 PORT=8135
 
-eval "/home/nodo/bin/monero-lws-daemon --daemon tcp://127.0.0.1:18082 --db-path $DATA_DIR --admin-rest-server http://127.0.0.1:${PORT}/admin --rest-server http://127.0.0.1:${PORT}/basic --auto-accept-creation"
+base="/home/nodo/bin/monero-lws-daemon --daemon tcp://127.0.0.1:18082 --db-path $DATA_DIR --admin-rest-server http://127.0.0.1:${PORT}/admin --rest-server http://127.0.0.1:${PORT}/basic"
+
+# in case version too old
+eval "$base --auto-accept-creation" \
+	|| eval "$base"
